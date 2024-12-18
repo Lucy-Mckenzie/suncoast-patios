f26237b6a59915c7fadd2002f4628af7ac46628a
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-18 19:51:10 +1300

    editing navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white py-4">
-    <ul className="flex justify-center space-x-6">
-      <li><Link href="/" className="hover:underline">HOME</Link></li>
-      <li><Link href="/products" className="hover:underline">PRODUCTS</Link></li>
-      <li><Link href="/gallery" className="hover:underline">GALLERY</Link></li>
-      <li><Link href="/contact" className="hover:underline">CONTACT US</Link></li>
-    </ul>
+    <nav>
+      <div className='flex items-center px-3 justify-center z-2 pb-4 pt-4 space-x-28'>
+      <div className='px-9 flex items-center space-x-28 no-underline'>
+        <Link href="/" className="hover:underline font-manrope hover:text-gray-500 z-20">HOME</Link>
+        <Link href="/products" className="hover:underline font-manrope hover:text-gray-500 z-20">PRODUCTS</Link>
+        <Link href="/gallery" className="hover:underline font-manrope hover:text-gray-500 z-20">GALLERY</Link>
+        <Link href="/contact" className="hover:underline font-manrope hover:text-gray-500 z-20">CONTACT US</Link>
+      </div>
+      </div>
   </nav>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-// landing page
+
 export default function Home() {
   return (
-    <>Home</>
-  );
+    <></>
+  )
 }
