05e88381768271f32dcb097b0835a1c47580c8e7
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-18 20:41:23 +1300

    styling navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-white">
-      <div className='flex items-center px-3 justify-center z-2 pb-4 pt-4'>
-      <div className='px-9 flex items-center space-x-28 no-underline'>
-        <Link href="/" className="hover:underline font-manrope hover:text-gray-500 z-20">HOME</Link>
-        <Link href="/products" className="hover:underline font-manrope hover:text-gray-500 z-20">PRODUCTS</Link>
-        <Link href="/gallery" className="hover:underline font-manrope hover:text-gray-500 z-20">GALLERY</Link>
-        <Link href="/contact" className="hover:underline font-manrope hover:text-gray-500 z-20">CONTACT US</Link>
+    <nav className='bg-[#fffbf9] relative'>
+      <div className='flex items-center justify-between z-2 pt-2 shadow-[3px_6px_10px_rgba(0,_0,_0,_0.04)]'>
+        <Image src='/suncoastlogo.png' alt='suncoast patios logo' width={160} height={50} className='ml-20 mr-auto z-1'/>
+      <div className='flex items-center space-x-12 no-underline pr-20'>
+        <Link href='/' className='hover:underline font-manrope hover:text-gray-500 z-20'>HOME</Link>
+        <Link href='/gallery' className='hover:underline font-manrope hover:text-gray-500 z-20'>GALLERY</Link>
+        <Link href='/contact' className='hover:underline font-manrope hover:text-gray-500 z-20'>CONTACT US</Link>
+        <button className='bg-[#FDB626] text-white p-2 rounded-md'>Free Measure and Quote</button>
       </div>
       </div>
   </nav>
