567cae9382edbe53e3455f5453d18f9ee9a3c0d8
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-18 20:20:57 +1300

    adding tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='w-full h-full '>
-      <Image src='/demo.png' alt='suncoast patios louvre' width={1300} height={600}/>
+    <div className='flex justify-center w-full'>
+      <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400}/>
     </div>
   )
 }
