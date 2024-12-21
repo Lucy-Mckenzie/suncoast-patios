f219706233cacc82304d73a9ecfd629dc7a31b14
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-21 14:36:49 +1300

    adding useRef and Usestate to carousel

--- src/app/components/PhotoCarousel.tsx
+++ src/app/components/PhotoCarousel.tsx
@@ -1,28 +1,78 @@
-import React from 'react'
+'use client'
+import React, { useRef, useState } from 'react'
 import Image from 'next/image'
 
-export default function PhotoCarousel() {
-  return (
-    <div className='carousel w-full overflow-hidden'>
-      <div id='carouselExample' className='carousel-inner flex'>
-        <div className='carousel-item w-full flex-shrink-0'>
-          <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400} className='w-full'/>
-        </div>
-        <div className='carousel-item w-full flex-shrink-0'>
-          <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400} className='w-full'/>
-        </div>
-        <div className='carousel-item w-full flex-shrink-0'>
-          <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400} className='w-full'/>
-        </div>
-      </div>
+const photos = [
+  '/demo.png',
+  '/demo.png',
+  '/demo.png',
+  '/demo.png',
+  '/demo.png',
+  '/demo.png'
+]
 
-      <button className='carousel-control-prev absolute top-1/2 left-0 transform -translate-y-1/2 px-4 py-2 bg-gray-800 text-white rounded-full'>
+export default function PhotoCarousel() {
+  const [photo, setPhoto] = useState(0)
+  const carouselRef = useRef<HTMLDivElement | null>(null)
+
+  const nextImage = () => {
+    setPhoto((photo) => (photo + 1) % photos.length)
+    if (carouselRef.current) {
+    carouselRef.current.scrollTo({
+      left: photo * carouselRef.current.offsetWidth,
+      behavior: 'smooth'
+    })
+  }
+  }
+
+  const prevImage = () => {
+    setPhoto((photo) => (photo - 1 + photos.length) % photos.length)
+    if (carouselRef.current) {
+    carouselRef.current.scrollTo({
+      left: photo * carouselRef.current.offsetWidth,
+      behavior: 'smooth'
+    })
+  }
+  }
+
+  return (
+    <div className="carousel w-full overflow-hidden relative">
+    <div ref={carouselRef} className="carousel-inner flex overflow-x-auto snap-x snap-mandatory">
+      {photos.map((photoSrc, index) => (
+        <div key={index} className="carousel-item w-full flex-shrink-0 snap-start">
+          <Image 
+            src={photoSrc} 
+            alt="Image carousel" 
+            width={1200} 
+            height={400} 
+            className="w-full"
+          />
+        </div>
+      ))}
+    </div>
+
+    <div className=''>
+      <button 
+        className='carousel-control-prev absolute top-1/2 left-60 transform -translate-y-1/2 px-4 py-2 bg-gray-800/70 text-white rounded-full'
+        onClick={prevImage}
+      >
         <span className='carousel-control-prev-icon'>&lt;</span>
       </button>
-      <button className='carousel-control-next absolute top-1/2 right-0 transform -translate-y-1/2 px-4 py-2 bg-gray-800 text-white rounded-full'>
+    </div>
+
+    <h1 className='text-6xl absolute z-10 text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-manrope'>
+      Ultimate Outdoor Living
+    </h1>
+
+    <div className=''>
+      <button 
+        className='carousel-control-next absolute top-1/2 right-60 transform -translate-y-1/2 px-4 py-2 bg-gray-800/70 text-white rounded-full'
+        onClick={nextImage}
+      >
         <span className='carousel-control-next-icon'>&gt;</span>
       </button>
     </div>
+  </div>
   )
 }
 
