4df0e9cafa824acff109f5c588a7d81a0e98ccca
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-23 12:40:28 +1300

    Adding form to component

--- src/app/components/form/EnquiryForm.tsx
+++ src/app/components/form/EnquiryForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Timeline from './Timeline'
+import Form from './Form'
 
 export default function EnquiryForm() {
   const [isOpen, setIsOpen] = useState(false)
@@ -67,61 +68,11 @@ export default function EnquiryForm() {
           </div> */}
    <Timeline />
     </div>
-           <form className='lg:w-2/3 bg-[#FDB626]/60 p-8 rounded-lg space-y-4 shadow-inner'>
-        <div className='form-control mb-3'>
-          <label className='label' htmlFor='name'>
-            <span className='label-text'>Full Name*</span>
-          </label>
-          <input
-            id='name'
-            type='text'
-            placeholder='Your full name'
-            className='input input-bordered w-full'
-            required
-          />
-        </div>
-        <div className='form-control mb-3'>
-          <label className='label'>
-            <span className='label-text'>Email*</span>
-          </label>
-          <input
-            id='email'
-            type='email'
-            placeholder='Your email'
-            className='input input-bordered w-full'
-            required
-          />
-        </div>
-
-        <div className='form-control mb-3'>
-          <label className='label'>
-            <span className='label-text'>Phone Number*</span>
-          </label>
-          <input
-            id='number'
-            type='tel'
-            placeholder='Your phone number'
-            className='input input-bordered w-full'
-            required
-          />
-        </div>
-        <div className='form-control mb-3'>
-          <label className='label'>
-            <span className='label-text'>Message</span>
-          </label>
-          <textarea
-            id='message'
-            className='textarea textarea-ghost w-full border-b-[1px] border-white'
-          ></textarea>
-        </div>
-        
-        <div className='form-control'>
-          <button className='btn bg-white text-[#FDB626] hover:bg-[#e09c1f] w-[150px] shadow-lg hover:scale-105'>Submit</button>
-        </div>
-      </form>
+    <div className='lg:w-2/3'>
+          <Form />
+      </div>
     </div>
     </div>
-   
     }
   </div>  
   )
---
+++ src/app/components/form/Form.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div>
+       <form className='bg-[#FDB626]/60 p-8 rounded-lg space-y-4 shadow-inner'>
+        <div className='form-control mb-3'>
+          <label className='label' htmlFor='name'>
+            <span className='label-text'>Full Name*</span>
+          </label>
+          <input
+            id='name'
+            type='text'
+            placeholder='Your full name'
+            className='input input-bordered w-full'
+            required
+          />
+        </div>
+        <div className='form-control mb-3'>
+          <label className='label'>
+            <span className='label-text'>Email*</span>
+          </label>
+          <input
+            id='email'
+            type='email'
+            placeholder='Your email'
+            className='input input-bordered w-full'
+            required
+          />
+        </div>
+
+        <div className='form-control mb-3'>
+          <label className='label'>
+            <span className='label-text'>Phone Number*</span>
+          </label>
+          <input
+            id='number'
+            type='tel'
+            placeholder='Your phone number'
+            className='input input-bordered w-full'
+            required
+          />
+        </div>
+        <div className='form-control mb-3'>
+          <label className='label'>
+            <span className='label-text'>Message</span>
+          </label>
+          <textarea
+            id='message'
+            className='textarea textarea-ghost w-full border-b-[1px] border-white'
+          ></textarea>
+        </div>
+        
+        <div className='form-control'>
+          <button className='btn bg-white text-[#FDB626] hover:bg-[#e09c1f] w-[150px] shadow-lg hover:scale-105'>Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
