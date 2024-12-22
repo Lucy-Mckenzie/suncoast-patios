e290442ba533b2f67c225e679c63e116bde23875
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-22 16:11:44 +1300

    adding custom font

--- src/app/components/form/EnquiryForm.tsx
+++ src/app/components/form/EnquiryForm.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import Timeline from './Timeline'
-import Form from './Form'
 
 export default function EnquiryForm() {
   const [isOpen, setIsOpen] = useState(false)
@@ -11,21 +10,21 @@ export default function EnquiryForm() {
       <label tabIndex={0} className='btn bg-[#FDB626] text-white hover:bg-[#e09c1f] transition-all duration-300'
         onClick={() => setIsOpen(!isOpen)}
       >
-    {isOpen ? 'Close Form' : 'Free Measure and Quote'}
+    { isOpen ? 'Close Form' : 'Free Measure and Quote'}
     </label>
     {isOpen && 
-
      <div
      className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50"
-     onClick={() => setIsOpen(false)}>
-
+     onClick={() => setIsOpen(false)}
+   >
      <div
        className="relative bg-white text-black flex flex-col lg:flex-row rounded-lg shadow-lg w-full max-w-5xl h-auto p-6 space-y-6 lg:space-y-0 lg:space-x-6"
-       onClick={(e) => e.stopPropagation()}>
-
+       onClick={(e) => e.stopPropagation()}
+     >
+      
    <div className='flex flex-col items-center space-y-8'>
-   <div className="w-full lg:w-1/2 space-y-4 text-center lg:text-left">
-      <h1 className='text-2xl font-bold'>
+   <div className="w-full lg:w-2/3 space-y-4 text-center lg:text-left">
+      <h1 className='text-2xl font-bold font-manrope'>
       The road to comfortable outdoor living starts here. We just need a few details.
       </h1>
       <h2 className='mb-2 font-medium'>Call us directly:</h2>
@@ -35,18 +34,70 @@ export default function EnquiryForm() {
     </div>
     <div>
       </div>
-  <div className='w-full text-center'>
-        <Image
-          src='/logos/form.png'
-          alt='enquiry form'
-          width={200}
-          height={200}
-          className='object-contain mx-auto' 
-        />
-      </div>
+      {/* <div className='w-full text-center'>
+            <Image
+              src='/logos/form.png'
+              alt='enquiry form'
+              width={200}
+              height={200}
+              className='object-contain mx-auto' 
+            />
+          </div> */}
    <Timeline />
     </div>
-      <Form />
+           <form className='lg:w-2/3 bg-[#FDB626]/60 p-8 rounded-lg space-y-4 shadow-inner'>
+        <div className='form-control mb-3'>
+          <label className='label' htmlFor="name">
+            <span className='label-text'>Full Name*</span>
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder='Your full name'
+            className='input input-bordered w-full'
+            required
+          />
+        </div>
+        <div className='form-control mb-3'>
+          <label className='label'>
+            <span className='label-text'>Email*</span>
+          </label>
+          <input
+            id='email'
+            type='email'
+            placeholder='Your email'
+            className='input input-bordered w-full'
+            required
+          />
+        </div>
+
+        <div className='form-control mb-3'>
+          <label className='label'>
+            <span className='label-text'>Phone Number*</span>
+          </label>
+          <input
+            id='number'
+            type='tel'
+            placeholder='Your phone number'
+            className='input input-bordered w-full'
+            required
+          />
+        </div>
+        <div className='form-control mb-3'>
+          <label className='label'>
+            <span className='label-text'>Message</span>
+          </label>
+          <textarea
+            id='message'
+            placeholder='Your message here'
+            className='textarea textarea-bordered w-full'
+          ></textarea>
+        </div>
+        
+        <div className='form-control'>
+          <button className='btn bg-white text-[#FDB626] hover:bg-[#e09c1f] w-[150px] shadow-lg hover:scale-105'>Submit</button>
+        </div>
+      </form>
     </div>
     </div>
    
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,6 @@ import PhotoCarousel from './components/PhotoCarouselMain'
 export default function Home() {
   return (
     <div className='flex flex-col w-full'>
-      {/* <div className='flex justify-center'>
-      <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400}/>
-      </div> */}
       <PhotoCarousel />
       <Products />
       <Reviews /> 
