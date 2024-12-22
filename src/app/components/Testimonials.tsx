c7f51cb111a318a7e0864944c3b481d821c1309a
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-23 12:16:50 +1300

    organising testimonials

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,97 +1,90 @@
 'use client'
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
-// type Review = {
-//   id: number;
-//   imageSrc: string;
-//   alt: string;
-// }
+type Review = {
+  id: number;
+  imageSrc: string;
+  alt: string;
+  author: string;
+  description: string;
+}
 
-// const reviews: Review[] = [
-//   { id: 1, imageSrc: '/demo.png', alt: 'Patio Image' },
-//   { id: 2, imageSrc: '/demo.png', alt: 'Louvers Image' },
-//   { id: 3, imageSrc: '/demo.png', alt: 'Blinds Image' },
-//   { id: 4, imageSrc: '/demo.png', alt: 'Patio Image 2' },
-//   { id: 5, imageSrc: '/demo.png', alt: 'Patio Image' },
-// ]
+const reviews: Review[] = [
+  { id: 1, imageSrc: '/demo.png', alt: 'Patio Image', author: 'Alex Kay', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+  { id: 2, imageSrc: '/demo.png', alt: 'Louvers Image', author: 'Alex Kay', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+  { id: 3, imageSrc: '/demo.png', alt: 'Blinds Image', author: 'Alex Kay', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+  { id: 4, imageSrc: '/demo.png', alt: 'Patio Image 2', author: 'Alex Kay', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+  { id: 5, imageSrc: '/demo.png', alt: 'Patio Image', author: 'Alex Kay', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+]
 
 export default function Reviews() {
-  // const [visibleReviews, setVisibleReviews] = useState(2)
+  const [visibleReviews, setVisibleReviews] = useState(4)
 
-  // const showMore = () => {
-  //   setVisibleReviews(reviews.length)
-  // }
+  const showMore = () => {
+    setVisibleReviews(reviews.length)
+  }
 
-  // const showLess = () => {
-  //   setVisibleReviews(2)
-  // }
+  const showLess = () => {
+    setVisibleReviews(4)
+  }
 
   return (
-<section className="py-10 bg-gray-100 sm:py-16 lg:py-24">
+ <section className="py-10 bg-gray-100 sm:py-16 lg:py-16"> 
     <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div className='flex flex-col items-center justify-center py-20'>
         <div className="max-w-2xl mx-auto text-center">
-            <h2 className="text-3xl font-bold leading-tight text-gray-800 sm:text-4xl lg:text-5xl">Trusted by <span className="text-blue-600">Hawkes Bay</span> for more than 12+ years </h2>
+            <h2 className="text-3xl font-bold font-manrope leading-tight text-gray-800 sm:text-4xl lg:text-5xl">Trusted by <span className="text-blue-600">Hawkes Bay</span> for more than 12+ years </h2>
+            </div>
+  
+    <div className='grid grid-cols-1 sm:grid-cols-4 gap-4 space-x-4 justify-items-center mx-auto pt-10'>
+        {reviews.slice(0, visibleReviews).map((review) => (
+           <div
+           key={review.id} 
+           className='card bg-base-100 shadow-md rounded-md'>
+          <figure className=''>
+            <Image 
+            src={review.imageSrc}
+            alt={review.alt}
+             width={500} 
+             height={300}
+             className='object-cover'
+             />
+          </figure>
+          <div className='card-body'>
+            <h2 className='card-title'>{review.author}</h2>
+            <div className='card-actions justify-start'>
+            <div className="rating rating-sm">
+            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+            </div>
+            </div>
+            <p>{review.description}</p>
+          </div>
         </div>
-
-        <div className="grid max-w-xl grid-cols-1 mx-auto mt-8 text-center lg:max-w-full sm:mt-12 lg:mt-20 lg:grid-cols-3 gap-x-6 xl:gap-x-12 gap-y-6">
-            <div className="overflow-hidden bg-white rounded-md shadow">
-                <div className="px-8 py-12">
-                    <div className="relative w-24 h-24 mx-auto">
-                        <Image className="relative object-cover w-24 h-24 mx-auto rounded-full" src="/demo.png" alt="" width={200} height={200}/>
-                        <div className="absolute top-0 right-0 flex items-center justify-center bg-blue-600 rounded-full w-7 h-7">
-                            <svg className="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                <path
-                                    d="M20.309 17.708C22.196 15.66 22.006 13.03 22 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292zm-11.007 0C11.19 15.66 10.999 13.03 10.993 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292z"
-                                ></path>
-                            </svg>
-                        </div>
-                    </div>
-                    <blockquote className="mt-7">
-                        <p className="text-lg text-black">“Amet minim mollit non deserunt ullam co est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat”</p>
-                    </blockquote>
-                    <p className="text-base font-semibold text-black mt-9">Robert Fox</p>
-                </div>
-            </div>
-
-            <div className="overflow-hidden bg-white rounded-md shadow">
-                <div className="px-8 py-12">
-                    <div className="relative w-24 h-24 mx-auto">
-                        <Image className="relative object-cover w-24 h-24 mx-auto rounded-full" src="/demo.png" alt=""  width={200} height={200} />
-                        <div className="absolute top-0 right-0 flex items-center justify-center bg-blue-600 rounded-full w-7 h-7">
-                            <svg className="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                <path
-                                    d="M20.309 17.708C22.196 15.66 22.006 13.03 22 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292zm-11.007 0C11.19 15.66 10.999 13.03 10.993 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292z"
-                                ></path>
-                            </svg>
-                        </div>
-                    </div>
-                    <blockquote className="mt-7">
-                        <p className="text-lg text-black">“Amet minim mollit non deserunt ullam co est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat”</p>
-                    </blockquote>
-                    <p className="text-base font-semibold text-black mt-9">Robert Fox</p>
-                </div>
-            </div>
-
-            <div className="overflow-hidden bg-white rounded-md shadow">
-                <div className="px-8 py-12">
-                    <div className="relative w-24 h-24 mx-auto">
-                        <Image className="relative object-cover w-24 h-24 mx-auto rounded-full" src="/demo.png" alt="" width={200} height={200} />
-                        <div className="absolute top-0 right-0 flex items-center justify-center bg-blue-600 rounded-full w-7 h-7">
-                            <svg className="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                <path
-                                    d="M20.309 17.708C22.196 15.66 22.006 13.03 22 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292zm-11.007 0C11.19 15.66 10.999 13.03 10.993 13V5a1 1 0 0 0-1-1h-6c-1.103 0-2 .897-2 2v7a1 1 0 0 0 1 1h3.078a2.89 2.89 0 0 1-.429 1.396c-.508.801-1.465 1.348-2.846 1.624l-.803.16V20h1c2.783 0 4.906-.771 6.309-2.292z"
-                                ></path>
-                            </svg>
-                        </div>
-                    </div>
-                    <blockquote className="mt-7">
-                        <p className="text-lg text-black">“Amet minim mollit non deserunt ullam co est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat”</p>
-                    </blockquote>
-                    <p className="text-base font-semibold text-black mt-9">Robert Fox</p>
-                </div>
-            </div>
-        </div>
+        ))}
+      </div>
+        <div className='mt-10'>
+      {visibleReviews < reviews.length ? (
+        <button 
+        onClick={showMore}
+          className='btn btn-primary'
+          >
+            View More
+          </button>
+       ) : (
+        <button 
+        onClick={showLess}
+          className='btn btn-primary'
+          >
+            View Less
+          </button>
+      )}
+      </div>
+    </div>
     </div>
 </section>
 
--- src/app/components/TradeMark.tsx
+++ src/app/components/TradeMark.tsx
@@ -3,15 +3,33 @@ import Image from 'next/image'
 
 export default function TradeMark() {
   return (
-    <div className='flex flex-col items-center pb-10'>
-      <h1 className='text-2xl text-center font-manrope mb-6'>
-        We are an Authorized Dealer Of Stratco & Ziptrak Outdoor Products
-      </h1>
-      <div className='flex flex-row justify-center items-center gap-10 pb-10'>
-         <Image src='/logos/outback.png' alt='outback logo' width={200} height={100} className="object-contain"/>
-         <Image src='/logos/ziptrak.png' alt='ziptrack logo' width={200} height={100} className="object-contain"/>
-      </div>
-      <button className='btn btn-wide bg-[#FDB626] text-white hover:bg-[#e09c1f]'>Free Quote</button>
+    <div className="flex flex-col items-center text-center bg-gray-50 py-10 px-4 rounded-lg shadow-md">
+    <h1 className="text-2xl md:text-3xl font-bold font-manrope mb-8">
+      We are an Authorized Dealer Of Stratco & Ziptrak Outdoor Products
+    </h1>
+    
+    <div className="flex flex-row justify-center items-center gap-8 mb-6">
+      <Image
+        src="/logos/outback.png"
+        alt="Outback logo"
+        width={150}
+        height={80}
+        className="object-contain"
+      />
+      <Image
+        src="/logos/ziptrak.png"
+        alt="Ziptrak logo"
+        width={150}
+        height={80}
+        className="object-contain"
+      />
     </div>
+    
+    <button className="btn btn-wide bg-[#FDB626] text-white hover:bg-[#e09c1f] py-3 px-6 rounded-md text-lg">
+      Free Quote
+    </button>
+  </div>
+  
+  
   )
 }
