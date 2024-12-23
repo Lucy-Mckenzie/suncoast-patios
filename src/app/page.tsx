8f435453a84f811f10d6134690af89745245a3d1
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-23 17:17:46 +1300

    Organising files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Products from './components/Products'
-import Reviews from './components/Testimonials'
-import TradeMark from './components/TradeMark'
-import FAQ from './components/FAQ'
-import PhotoCarousel from './components/PhotoCarouselMain'
-import Steps from './components/Steps'
+import Products from './components/home/Products'
+import Reviews from './components/home/Testimonials'
+import TradeMark from './components/home/TradeMark'
+import FAQ from './components/home/FAQ'
+import PhotoCarousel from './components/home/PhotoCarouselMain'
+import Steps from './components/home/Steps'
 
 export default function Home() {
   return (
