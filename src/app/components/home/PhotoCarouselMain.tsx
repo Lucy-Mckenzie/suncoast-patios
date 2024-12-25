b328d16fe5fb2e528df47c0c71b1294dd843b0a8
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-25 19:45:01 +1300

    Chaning width of carousel, shadow on navbar, root color

--- src/app/components/home/PhotoCarouselMain.tsx
+++ src/app/components/home/PhotoCarouselMain.tsx
@@ -13,7 +13,7 @@ const photos = [
 
 export default function PhotoCarousel() {
   return (
-<div className="carousel w-full h-auto max-h-[1000px] overflow-hidden mx-auto relative">
+<div className='carousel max-w-[1450px] h-auto max-h-[1000px] overflow-hidden mx-auto relative rounded-lg'>
   {photos.map((photoSrc, index) => {
     const prevSlide = (index - 1 + photos.length) % photos.length + 1
     const nextSlide = (index + 1) % photos.length + 1
@@ -22,21 +22,21 @@ export default function PhotoCarousel() {
       <div
         key={index}
         id={`slide${index + 1}`}
-        className="carousel-item relative w-full"
+        className='carousel-item relative w-full'
       >
         <Image
           src={photoSrc}
           alt={`Slide ${index + 1}`}
           width={1200}
           height={400}
-          className="w-full"
+          className='w-full'
         />
 
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href={`#slide${prevSlide}`} className="btn btn-circle">
+        <div className='absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between'>
+          <a href={`#slide${prevSlide}`} className='btn btn-circle'>
             ❮
           </a>
-          <a href={`#slide${nextSlide}`} className="btn btn-circle">
+          <a href={`#slide${nextSlide}`} className='btn btn-circle'>
             ❯
           </a>
         </div>
--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -16,7 +16,7 @@ if (!context) {
 const { changedTheme } = context;
 
   return (
-    <nav className='navbar shadow-md z-100'>
+    <nav className='navbar shadow-sm z-100'>
     <div className='flex items-center justify-between w-full'>
       <div className='ml-4'>
          <Link
