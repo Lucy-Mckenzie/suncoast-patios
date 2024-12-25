a2d141a313c6b612e39c6ec695913de048fd3144
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-25 21:49:38 +1300

    Adding fake data related, adding links to buttons

--- src/app/components/home/Products.tsx
+++ src/app/components/home/Products.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Products() {
   return (
@@ -10,37 +11,82 @@ export default function Products() {
       <div className='flex flex-row flex-wrap justify-center space-x-8'>
       <div className='card bg-base-100 w-96 shadow-md rounded-md'>
   <figure className='px-10 pt-10'>
-      <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400}/>
+      <Image
+       src='/demo.png' 
+       alt='suncoast patios louvre'
+       width={1200}
+       height={400}
+       />
   </figure>
   <div className='card-body items-center text-center'>
-    <h2 className='card-title'></h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+    <h2 className='card-title'>
+      Louvres
+      </h2>
+    <p>
+    Louvre roofs in the sunny Hawke’s Bay create a combination to shape our ideal form of outdoor living. 
+    </p>
     <div className='card-actions'>
-      <button className='btn btn-primary'>View Product</button>
+    <Link
+       href='/products/louvres'>
+        <button
+         className='btn btn-primary'>
+          View Product
+        </button>
+      </Link>
     </div>
   </div>
 </div>
 <div className='card bg-base-100 w-96 shadow-md rounded-md'>
   <figure className='px-10 pt-10'>
-      <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400}/>
+      <Image
+       src='/demo.png'
+       alt='suncoast patios louvre'
+       width={1200}
+       height={400}
+       />
   </figure>
   <div className='card-body items-center text-center'>
-    <h2 className='card-title'></h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+    <h2 className='card-title'>
+      Patios
+    </h2>
+    <p>
+    The protection from the elements, stylish aesthetics and build quality designed to stand the test of time.
+    </p>
     <div className='card-actions'>
-      <button className='btn btn-primary'>View Product</button>
+    <Link
+       href='/products/patios'>
+        <button
+         className='btn btn-primary'>
+          View Product
+        </button>
+      </Link>
     </div>
   </div>
 </div>
 <div className='card bg-base-100 w-96 shadow-md rounded-md'>
   <figure className='px-10 pt-10'>
-      <Image src='/demo.png' alt='suncoast patios louvre' width={1200} height={400}/>
+      <Image
+       src='/demo.png'
+       alt='suncoast patios louvre'
+       width={1200}
+       height={400}
+       />
   </figure>
   <div className='card-body items-center text-center'>
-    <h2 className='card-title'></h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+    <h2 className='card-title'>
+      Blinds
+    </h2>
+    <p>
+    We believe the addition of outdoor blinds can completely transform any outdoor living space.
+    </p>
     <div className='card-actions'>
-      <button className='btn btn-primary'>View Product</button>
+      <Link
+       href='/products/blinds'>
+        <button
+         className='btn btn-primary'>
+          View Product
+        </button>
+      </Link>
     </div>
   </div>
 </div>
