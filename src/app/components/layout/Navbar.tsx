62bb33345df92438d3c6034ab4dda48b2417b8c4
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-26 08:54:44 +1300

    adding hamburger menu for sm screens

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -18,6 +18,45 @@ const { changedTheme } = context;
   return (
 <div className="navbar bg-base-100">
   <div className="navbar-start flex items-center">
+  <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+         <Link
+         href="/contact" 
+         className="transition">
+        Contact Us
+        </Link>
+        <li>
+          <a>Products</a>
+          <ul className="p-2">
+          <li>
+          <Link href="/products/louvres">Louvres</Link>
+        </li>
+        <li>
+          <Link href="/products/patios">Patios</Link>
+        </li>
+        <li>
+          <Link href="/products/blinds">Blinds</Link>
+        </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
     <div className="ml-4">
       <Link href="/">
         <Image
@@ -40,7 +79,7 @@ const { changedTheme } = context;
     </li>
       <li>
         <details>
-          <summary>Parent</summary>
+          <summary>Products</summary>
           <ul
         tabIndex={0}
         className="dropdown-content menu menu-sm rounded-box z-[1] mt-2 w-40 bg-base-100 p-2 shadow">
