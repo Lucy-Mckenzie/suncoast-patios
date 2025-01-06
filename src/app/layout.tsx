df9d60e3c95930ecb29a1a7fb12f14b768a4594a
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-06 14:31:11 +1300

    adding seo metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import ClientThemeWrapper from './context/ClientThemeWrapper';
 import ThemeProvider from './context/ThemeContext';
 import './globals.css';
 import Script from 'next/script';
-
 import { Manrope } from 'next/font/google'
+import Head from 'next/head';
 
 const manrope = Manrope({
   subsets: ['latin'],
@@ -19,10 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' className={manrope.className}>
+        <Head>
+        <title>Suncoast Patios</title>
+        <meta name="description" content="Transform your outdoor spaces with Suncoast Patios Ltd, serving Hawke's Bay and Gisborne. Specializing in high-quality patio roofs, louvre roofs, and outdoor blinds, we provide Stratco and Ziptrak solutions for year-round comfort and style. Enhance your outdoor living today!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
     <body>
     <Script
           src={`https://www.google.com/recaptcha/enterprise.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_KEY}`}
-          strategy="beforeInteractive"
+          strategy='beforeInteractive'
         />
       <ThemeProvider>
         <ClientThemeWrapper>
