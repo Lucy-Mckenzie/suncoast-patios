89e2469c8d060f6c0c35dae8ce69f0409e9d4bd5
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-07 14:45:05 +1300

    adding accessibility to form

--- src/app/components/form/EnquiryForm.tsx
+++ src/app/components/form/EnquiryForm.tsx
@@ -9,50 +9,73 @@ export default function EnquiryForm() {
 
   return (
     <div className='dropdown dropdown-end relative'>
-      <label tabIndex={0} className='btn bg-[#FDB626] text-base-200 hover:bg-[#e09c1f] transition-all duration-300 font-manrope rounded-full'
-        onClick={() => setIsOpen(!isOpen)}
+      <label 
+       id='dropdown-button'
+       tabIndex={0} 
+       className='btn bg-[#FDB626] text-base-200 hover:bg-[#e09c1f] transition-all duration-300 font-manrope rounded-full'
+       aria-expanded={isOpen}
+       aria-controls='dropdown-content'
+       onClick={() => setIsOpen(!isOpen)}
+       onKeyDown={(e) => {
+        if (e.key === 'Escape' && isOpen) setIsOpen(false)
+      }}
       >
     { isOpen ? 'Close Form' : 'Free Quote'}
     </label>
     {isOpen && 
      <div
+     id='dropdown-content'
      className='fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50'
+     role='dialog'
+     aria-labelledby='dropdown-title'
+     aria-describedby='dropdown-description'
      onClick={() => setIsOpen(false)}
    >
      <div
        className='relative bg-base-100 text-base-content flex flex-col lg:flex-row rounded-lg shadow-lg lg:w-full overflow-scroll lg:max-w-5xl lg:max-h-full max-h-[90vh] max-w-xs p-6 space-y-6 lg:space-y-0 lg:space-x-6 my-5'
-       onClick={(e) => e.stopPropagation()}
-     >
-      
-   <div className='lg:flex lg:flex-col items-center lg:space-y-4'>
-   <div className='lg:w-2/3 text-center lg:text-left'>
-  <h1 className='lg:text-2xl text-lg font-bold font-manrope lg:mb-16'>
-    The road to comfortable outdoor living starts here. We just need a few details.
-  </h1>
+       onClick={(e) => e.stopPropagation()}>
+<div className='lg:flex lg:flex-col items-center lg:space-y-4'>
+  <div className='lg:w-2/3 text-center lg:text-left'>
+    <h1 
+      id='dropdown-title'
+      className='lg:text-2xl text-lg font-bold font-manrope lg:mb-16'>
+      The road to comfortable outdoor living starts here. We just need a few details.
+   </h1>
+   <p id='dropdown-description' className='sr-only'>
+      Fill out the form to request a free quote.
+    </p>
 
   <div className='flex items-center lg:space-x-3 space-x-2 mt-4 mb-2'>
   <Image
     src='/logos/phone.png'
-    alt='enquiry form'
+    alt='Phone Icon'
     width={20}
     height={20}
     className='object-contain lg:h-5 lg:w-5 h-3 w-3'
   />
-    <h2 className='font-medium lg:text-xl text-md'>Call us directly:</h2>
-    <p className='text-content lg:text-lg text-sm'>+64 22 170 9933</p>
+    <h2 className='font-medium lg:text-xl text-md'>
+      Call us directly:
+      </h2>
+    <p className='text-content lg:text-lg text-sm'>
+      +64 22 170 9933
+      </p>
   </div>
 
 
 <div className='flex items-center lg:space-x-3 space-x-2 mb-4'>
   <Image
     src='/logos/mail.png'
-    alt='enquiry form'
+    alt='Mail Icon'
     width={20}
     height={20}
     className='object-contain lg:h-5 lg:w-5 h-3 w-3'
   />
-    <h2 className='font-medium lg:text-xl text-md'>Email:</h2>
-    <p className='text-content lg:text-lg text-sm'>info@suncoastpatios.co.nz</p>
+    <h2 className='font-medium lg:text-xl text-md'>
+      Email:
+      </h2>
+    <p className='text-content lg:text-lg text-sm'>
+      info@suncoastpatios.co.nz
+      </p>
   </div>
 </div>
 
@@ -61,7 +84,7 @@ export default function EnquiryForm() {
       <div className='w-full text-center'>
             <Image
               src='/logos/form.png'
-              alt='enquiry form'
+              alt='Enquiry Form Drawing'
               width={200}
               height={200}
               className='object-contain mx-auto lg:h-52 lg:w-52 h-28 w-28' 
