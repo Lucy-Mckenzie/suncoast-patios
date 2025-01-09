55abdc06fd8deb8dfded5b9cc60af9ddc0b3b595
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-09 13:07:58 +1300

    adding privacy images

--- src/app/components/products/features/blinds/Privacy.tsx
+++ src/app/components/products/features/blinds/Privacy.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Privacy() {
   return (
     <div>
+    <div className='relative grid grid-cols-2 items-center justify-center w-full h-[550px]'>
+       <Image
+         src='/blinds/blinds5.webp'
+         alt='privacy inside'
+         width={400}
+         height={500}
+         className='object-cover w-full h-full rounded-md'
+       />
+        <Image
+         src='/blinds/blinds7.webp'
+         alt='privacy outside'
+         width={400}
+         height={500}
+         className='object-cover w-full h-full rounded-md'
+       />
+     </div>  
    
     </div>
   )
