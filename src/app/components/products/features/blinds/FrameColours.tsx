500dadbbc99fdd07e80c93b32b51a95b1fd6dc20
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-11 11:32:57 +1300

    adding link between colours and images

--- src/app/components/products/features/blinds/FrameColours.tsx
+++ src/app/components/products/features/blinds/FrameColours.tsx
@@ -3,61 +3,73 @@ import Image from 'next/image'
 import Tooltip from '../Tooltip'
 
 const photos = [
-  '/blinds/blinds.webp',
-  '/blinds/blinds1.webp',
-  '/blinds/blinds3.webp',
-  '/blinds/blinds4.webp',
-  '/blinds/blinds10.webp',
-  '/blinds/blinds12.webp',
+  { photo: '/blinds/blinds.webp',  colour: 'Gull Grey'},
+  { photo: '/blinds/blinds1.webp', colour: 'Basalt'},
+  { photo: '/blinds/blinds3.webp', colour: 'Blackstone'},
+  { photo: '/blinds/blinds4.webp', colour: 'Mocha'},
+  { photo: '/blinds/blinds10.webp', colour: 'Platinum'},
+  { photo: '/blinds/blinds12.webp', colour: 'Storm'}
 ]
 
 export default function FrameColours() {
   const [selectedColor, setSelectedColor] = useState<string | null>(null)
 
   const frameColours = [
-    { color: '#f2f4f5', label: 'Gull Grey' },
-    { color: '#f9e9ca', label: 'Basalt' },
-    { color: '#e5d2b4', label: 'Blackstone' },
-    { color: '#4f5650', label: 'Mocha' },
-    { color: '#404641', label: 'Platinum' },
-    { color: '#001722', label: 'Storm' },
+    { colour: '#f2f4f5', label: 'Gull Grey' },
+    { colour: '#f9e9ca', label: 'Basalt' },
+    { colour: '#e5d2b4', label: 'Blackstone' },
+    { colour: '#4f5650', label: 'Mocha' },
+    { colour: '#404641', label: 'Platinum' },
+    { colour: '#001722', label: 'Storm' },
   ]
+
+  const handleColorSelect = (colorLabel: string) => {
+    setSelectedColor(colorLabel)
+  }
   
   return (
     <div className='relative flex flex-col items-center justify-center w-full lg:h-[550px] h-auto'>
-      <div className='space-y-4 my-6'>
-        <div className='flex gap-4 items-center justify-center'>
-          {frameColours.map((colorOption, index) => (
-            <div key={index} className='relative group'>
-              <div
-                className={`lg:w-14 lg:h-14 w-9 h-9 rounded-full shadow-sm cursor-pointer`}
-                onClick={() => setSelectedColor(colorOption.color)}
-              />
-              <Tooltip tooltip={colorOption.label} />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className='relative w-full overflow-hidden rounded-md'>
-        <div className='grid grid-cols-3 grid-rows-2 sm:grid-cols-3 gap-1'>
-          {photos.map((photoSrc, index) => (
+    <div className='space-y-4 my-6'>
+      <div className='flex gap-4 items-center justify-center'>
+        {frameColours.map((colorOption, index) => (
+          <div
+            key={index}
+            className='relative group'
+            onClick={() => handleColorSelect(colorOption.label)}
+          >
             <div
-              key={index}
-              className={`w-full h-auto ${selectedColor ? `border-4 border-[${selectedColor}]` : ''}`}
-            >
-              <Image
-                src={photoSrc}
-                alt={`Slide ${index + 1}`}
-                width={400}
-                height={200}
-                className='rounded-md'
-              />
-            </div>
-          ))}
-        </div>
+              style={{ backgroundColor: colorOption.colour }}
+              className={`lg:w-14 lg:h-14 w-9 h-9 rounded-full shadow-sm cursor-pointer ${selectedColor === colorOption.label ? 'border-4 border-[#FDB626]' : ''}`}
+            />
+            <Tooltip tooltip={colorOption.label} />
+          </div>
+        ))}
       </div>
     </div>
+    <div className='relative w-full overflow-hidden rounded-md'>
+      <div className='grid lg:grid-cols-3 grid-rows-2 grid-cols-2 gap-2'>
+        {photos.map((photo, index) => (
+          <div
+            key={index}
+            className={`w-full h-auto ${
+              selectedColor === photo.colour ? 'border-4 rounded-lg border-[#FDB626]' : ''
+            }`} 
+          >
+            <Image
+              src={photo.photo}
+              alt={photo.colour}
+              width={400}
+              height={200}
+              className='rounded-md'
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
   )
 }
+
+
 
 
