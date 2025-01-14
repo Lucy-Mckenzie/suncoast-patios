ab28573049eab703413b11edd16ba4f90b354863
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-14 13:31:13 +1300

    adding louvres features

--- src/app/components/products/FeaturesLouvres.tsx
+++ src/app/components/products/FeaturesLouvres.tsx
@@ -1,33 +1,42 @@
 'use client'
-import React from 'react'
+import React, { JSX } from 'react'
 import { useState } from 'react'
-import Image from 'next/image'
+import ColourRange from './features/louvres/ColourRange'
+import Warranty from './features/louvres/Warranty'
+import KudosLouvres from './features/louvres/kudos'
+import RainSensor from './features/louvres/RainSensor'
+
+const FeatureComponents: { [key: string]: () => JSX.Element } = {
+'Kudos Louvres': () => <KudosLouvres />,
+Warranty: () => <Warranty />,
+'Colour Range': () => <ColourRange />,
+'Rain Sensor': () => <RainSensor />,
+}
 
 const features = [
-  { id: 1, name: 'Colours', image: '/demo1.png' },
-  { id: 2, name: '165 Degree Rotation', image: '/demo.png' },
-  { id: 3, name: 'Remote Opening', image: '/demo1.png' },
-  { id: 4, name: 'Guaranteed 15 Years', image: '/demo.png' },
-  { id: 5, name: 'Auto Closures in Rain', image: '/demo1.png' },
-  { id: 6, name: 'Any Size Achievable', image: '/demo.png' },
+  { id: 1, name: "Kudos Louvres", description: "Crafted from high-quality aluminum, these opening roofs are durable and built to withstand the elements. These innovative roofs can be operated manually or with the touch of a button."  },
+  { id: 2, name: "Warranty", description: "Built to last and backed by a comprehensive warranty for long-term reliability and peace of mind."   },
+  { id: 3, name: "Colour Range", description: "Customers can choose from a range of colours and finishes to ensure their opening roof fits seamlessly with the existing aesthetic of the property."  },
+  { id: 4, name: "Rain Sensor", description: "The louvre system has an in-built rain sensor that will automatically close the blades if rain is detected (motorised units only)."  },
 ]
 
-export default function FeaturesLouvres ()  {
+export default function FeaturesLouvres()  {
   const [activeFeature, setActiveFeature] = useState(features[0])
+  const ActiveFeatureComponent = FeatureComponents[activeFeature.name] || (() => <div>Feature not available</div>)
 
   return (
 <div>
-<div className='max-w-7xl mx-auto p-8 bg-base shadow-lg rounded-md'>
+<div className='mx-auto lg:p-8 p-4 bg-base shadow-lg border-[1px] rounded-md'>
   <div className='flex flex-col lg:flex-row'>
   
-          <div className='flex flex-col gap-4 lg:w-1/3 bg-base-200 rounded-md'>
+          <div className='flex flex-col gap-4 bg-base-100 rounded-md'>
             {features.map((feature) => (
               <button
                 key={feature.id}
-                className={`p-4 text-left rounded-md ${
-                  activeFeature.id === feature.id
-                    ? 'bg-base-300'
-                    : 'bg-base-200 hover:bg-base-100'
+                className={`p-4 text-left rounded-md w-64
+                  ${ activeFeature.id === feature.id
+                  ? 'bg-base-300 text- font-bold shadow-md border-[1px]'
+                  : 'bg-base-200 hover:bg-base-100 text-base-content' 
                 }`}
                 onClick={() => setActiveFeature(feature)}
               >
@@ -36,14 +45,10 @@ export default function FeaturesLouvres ()  {
             ))}
           </div>
   
-          <div className='mt-8 lg:mt-0 lg:w-2/3 lg:pl-4 flex justify-center items-center'>
-            <Image
-              src={activeFeature.image}
-              alt={activeFeature.name}
-              className='h-[500px] rounded-md shadow-md'
-              width={1500}
-              height={1500}
-            />
+          <div className={`lg:max-w-4xl bd-base-300 flex flex-col items-start justify-center w-full lg:h-[620px] h-auto lg:pl-6  lg:pt-0 pt-4`}>
+          <h2 className='text-xl font-semibold my-1 text-left'>{activeFeature.name}</h2>
+          <p className='text-md text-base mb-4 text-left border-b-[1px]'>{activeFeature.description}</p>
+          <ActiveFeatureComponent />
           </div>
         </div>
       </div>
