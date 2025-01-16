d54601bba410cd7971518d1e5797fe2da593eb91
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-17 09:59:28 +1300

    Adjusting trademark

--- src/app/components/home/TradeMark.tsx
+++ src/app/components/home/TradeMark.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import Image from 'next/image'
 import EnquiryForm from '../form/EnquiryForm'
@@ -7,29 +6,26 @@ export default function TradeMark() {
   return (
     <div className='flex flex-col items-center text-center bg-base-100 py-10 px-4 rounded-lg'>
     <h1 className='text-2xl md:text-3xl font-bold font-manrope mb-8'>
-      We are an Authorized Dealer Of Stratco & Ziptrak Outdoor Products
+    Official Dealer of Stratco & Ziptrak Outdoor Solutions
     </h1>
-    
     <div className='flex flex-row justify-center items-center gap-8 mb-6'>
       <Image
         src='/logos/outback.png'
         alt='Outback logo'
-        width={150}
+        width={350}
         height={80}
         className='object-contain'
       />
       <Image
         src='/logos/ziptrak.png'
         alt='Ziptrak logo'
-        width={150}
+        width={350}
         height={80}
         className='object-contain'
       />
     </div>
-    
   <EnquiryForm />
   </div>
-  
   
   )
 }
