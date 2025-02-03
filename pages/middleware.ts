a4490d7819f7c0e63350c0ed6cbc3b756702a0c8
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-02-04 09:12:28 +1300

    remvoing middleware.

--- pages/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware(req: Request) {
-  const host = req.headers.get('host') || ''
-  if (host === 'suncoastpatios.co.nz') {
-    return NextResponse.redirect('https://www.suncoastpatios.co.nz' + req.url, 301)
-  }
-}
