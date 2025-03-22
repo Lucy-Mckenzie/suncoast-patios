e0b7353b1a523096c5861d6b6554478641158321
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-03-23 09:18:24 +1000

    Adding page speed insights, changing deploy to vercel.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Manrope } from 'next/font/google'
 import { Metadata } from 'next'
 import React from 'react'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const manrope = Manrope({
   subsets: ['latin'],
@@ -42,6 +43,7 @@ export default function RootLayout({
             <main>
               {children}
               <GoogleAnalytics gaId="G-YGMY099QWP" />
+              <SpeedInsights />
             </main>
             <Footer />
           </ClientThemeWrapper>
